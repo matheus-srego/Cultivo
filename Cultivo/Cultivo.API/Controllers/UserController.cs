0b8d62b6c481eabc1e38653f2a5c564c6d2352a8
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 01:20:32 -0300

    Movendo método de inserção de usuário para a controladora de usuário

---
+++ Cultivo/Cultivo.API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Cultivo.Domain.DTOs;
+using Cultivo.Domain.Factories;
+using Cultivo.Domain.Interfaces.Services;
+using Cultivo.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cultivo.API.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] NewUserDTO newUser)
+        {
+            return Ok(await _userService.CreateAsync(UserFactory.Create(newUser)));
+        }
+    }
+}
