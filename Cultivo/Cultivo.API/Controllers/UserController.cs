00523c1253793c93c04e102cce6c358415382383
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 14:44:51 -0300

    Criando endpoint de Update para usuário

--- Cultivo/Cultivo.API/Controllers/UserController.cs
+++ Cultivo/Cultivo.API/Controllers/UserController.cs
@@ -4,11 +4,13 @@ using Cultivo.Domain.Interfaces.Services;
 using Cultivo.Domain.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace Cultivo.API.Controllers
 {
     [Route("api/user")]
     [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
@@ -25,6 +27,18 @@ namespace Cultivo.API.Controllers
         public async Task<IActionResult> Post([FromBody] NewUserDTO newUser)
         {
             return Ok(await _userService.CreateAsync(UserFactory.Create(newUser)));
+        }
+
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateUserDTO updateUser)
+        {
+            var user = await _userService.GetByIdAsync(updateUser.Id);
+            var userUpdated = UserFactory.Update(updateUser, user);
+
+            return Ok(await _userService.UpdateAsync(userUpdated));
         }
     }
 }
