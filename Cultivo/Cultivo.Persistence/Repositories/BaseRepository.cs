e611d5f28857ac8641dcf19ab08c372c79bca9ea
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 00:20:31 -0300

    Criando implementação de repositório abstrato e usuário

---
+++ Cultivo/Cultivo.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using Cultivo.Domain.Interfaces.Repositories;
+using Cultivo.Domain.Models;
+using Cultivo.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Persistence.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly CultivoContext _context;
+
+        public BaseRepository(CultivoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
---
+++ Cultivo/Cultivo.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Cultivo.Domain.Interfaces.Repositories;
+using Cultivo.Domain.Models;
+using Cultivo.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Persistence.Repositories
+{
+    public sealed class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(CultivoContext context) : base(context)
+        {
+            // ---
+        }
+    }
+}
