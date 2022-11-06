57a3196578b79c1718981c26013f7d88f0bdba46
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 14:43:59 -0300

    Criando implementação dos métodos GetByIdAsync e UpdateAsync no repositporio abstrato

--- Cultivo/Cultivo.Persistence/Repositories/BaseRepository.cs
+++ Cultivo/Cultivo.Persistence/Repositories/BaseRepository.cs
@@ -12,16 +12,31 @@ namespace Cultivo.Persistence.Repositories
 {
     public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
     {
+        protected readonly DbSet<T> _query;
         protected readonly CultivoContext _context;
 
         public BaseRepository(CultivoContext context)
         {
             _context = context;
+            _query = _context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _query.FindAsync(id);
         }
 
         public async Task<T> CreateAsync(T entity)
         {
-            await _context.AddAsync(entity);
+            await _query.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _query.Update(entity);
             await _context.SaveChangesAsync();
 
             return entity;
