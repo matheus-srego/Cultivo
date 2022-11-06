7ef6a9ba96950babcbfb7e8505de32086f4a8d61
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 00:19:18 -0300

    Criando DTO para criação de usuário

---
+++ Cultivo/Cultivo.Domain/DTOs/NewUserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Domain.DTOs
+{
+    public class NewUserDTO
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhotoURL { get; set; }
+
+        public NewUserDTO(string name, string lastName, string email, string password, string photoURL)
+        {
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            PhotoURL = photoURL;
+        }
+    }
+}
