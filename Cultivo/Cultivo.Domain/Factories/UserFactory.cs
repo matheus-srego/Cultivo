ad86baf730abe0b0395f32c7526a99774ccbc3b0
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 00:19:28 -0300

    Criando Factrory para usuário

---
+++ Cultivo/Cultivo.Domain/Factories/UserFactory.cs
@@ -0,0 +1,26 @@
+﻿using Cultivo.Domain.DTOs;
+using Cultivo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Domain.Factories
+{
+    public static class UserFactory
+    {
+        public static User Create(NewUserDTO newUser)
+        {
+            User user = new User();
+            
+            user.Name = newUser.Name;
+            user.LastName = newUser.LastName;
+            user.Email = newUser.Email;
+            user.Password = newUser.Password;
+            user.PhotoURL = newUser.PhotoURL;
+
+            return user;
+        }
+    }
+}
