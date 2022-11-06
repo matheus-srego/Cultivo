380a13d900eab647c598d48dff027c0fb7861ca5
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 00:19:41 -0300

    Criando contexto do projeto

---
+++ Cultivo/Cultivo.Persistence/Context/CultivoContext.cs
@@ -0,0 +1,33 @@
+﻿using Cultivo.Domain.Models;
+using Cultivo.Persistence.Mappers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Persistence.Context
+{
+    public class CultivoContext : DbContext
+    {
+        protected DbSet<User> User { get; set; }
+
+        public CultivoContext(DbContextOptions<CultivoContext> options) : base(options)
+        {
+            // ---
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMapper().Configure);
+        }
+    }
+}
