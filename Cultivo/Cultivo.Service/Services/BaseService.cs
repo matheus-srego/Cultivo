62bbfd4dabca4ececcd8aeaa1d81538966c99583
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 00:20:52 -0300

    Criando implementações de serviços abstrato e de usuário

---
+++ Cultivo/Cultivo.Service/Services/BaseService.cs
@@ -0,0 +1,23 @@
+﻿using Cultivo.Domain.Interfaces.Repositories;
+using Cultivo.Domain.Interfaces.Services;
+using Cultivo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Service.Services
+{
+    public class BaseService<T> : IBaseService<T> where T : BaseEntity
+    {
+        private readonly IBaseRepository<T> _baseRepository;
+
+        public BaseService(IBaseRepository<T> baseRepository)
+        {
+            _baseRepository = baseRepository;
+        }
+
+        public async Task<T> CreateAsync(T entity) => await _baseRepository.CreateAsync(entity);
+    }
+}
---
+++ Cultivo/Cultivo.Service/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Cultivo.Domain.Interfaces.Repositories;
+using Cultivo.Domain.Interfaces.Services;
+using Cultivo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Service.Services
+{
+    public sealed class UserService : BaseService<User>, IUserService
+    {
+        public UserService(IUserRepository userRepository) : base(userRepository)
+        {
+            // ---
+        }
+    }
+}
