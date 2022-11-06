b55314f8315f13e2042874321aeb5439a0d66d24
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 00:21:37 -0300

    Configurando implementações e interfaces de serviços e de repositórios

---
+++ Cultivo/Cultivo.API/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Cultivo.Domain.Interfaces.Repositories;
+using Cultivo.Domain.Interfaces.Services;
+using Cultivo.Persistence.Context;
+using Cultivo.Persistence.Repositories;
+using Cultivo.Service.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cultivo.API
+{
+    public class Startup
+    {
+        protected readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public virtual void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IUserService, UserService>();
+            serviceCollection.AddScoped<IUserRepository, UserRepository>();
+
+            var connection = _configuration["ConnectionString:cultivo-db"];
+
+            serviceCollection.AddDbContext<CultivoContext>(options =>
+            {
+
+                options.UseSqlServer(connection);
+
+            });
+        }
+    }
+}
