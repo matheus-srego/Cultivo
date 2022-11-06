6dd72a419ee8a98b4ccd855871100e244d46d0c7
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 00:19:04 -0300

    Criando interfaces de repositório abstrato e de usuário

---
+++ Cultivo/Cultivo.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Cultivo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<T> CreateAsync(T entity);
+    }
+}
