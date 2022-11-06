4307a3f0fffac708d783cdaf3011fa52db932a43
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 01:18:27 -0300

    Criando implementação de serviço para geração de token

---
+++ Cultivo/Cultivo.Service/Services/AuthService.cs
@@ -0,0 +1,36 @@
+﻿using Cultivo.Domain.Interfaces.Services;
+using Cultivo.Domain.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Service.Services
+{
+    public class AuthService : IAuthService
+    {
+        public string GenerateToken(Login login)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("ZWRpw6fDo28gZW0gY29tcHV0YWRvcmE");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Email, login.Email.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
