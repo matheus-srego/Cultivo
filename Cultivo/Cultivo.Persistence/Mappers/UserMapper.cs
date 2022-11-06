6d4a84a1428eef6a68580e236d7ecbd9a6bbdf3d
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-06 00:20:01 -0300

    Criando Mapper para mapear os atributos da tabela usuário

---
+++ Cultivo/Cultivo.Persistence/Mappers/UserMapper.cs
@@ -0,0 +1,45 @@
+﻿using Cultivo.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Persistence.Mappers
+{
+    public class UserMapper : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("usuario");
+
+            builder.HasKey(model => model.Id);
+
+            builder.Property(model => model.Id)
+                   .HasColumnName("id")
+                   .ValueGeneratedOnAdd()
+                   .IsRequired();
+
+            builder.Property(model => model.Name)
+                   .HasColumnName("nome")
+                   .IsRequired();
+
+            builder.Property(model => model.LastName)
+                   .HasColumnName("sobrenome")
+                   .IsRequired();
+
+            builder.Property(model => model.Email)
+                   .HasColumnName("email")
+                   .IsRequired();
+
+            builder.Property(model => model.Password)
+                   .HasColumnName("senha")
+                   .IsRequired();
+
+            builder.Property(model => model.PhotoURL)
+                   .HasColumnName("url_foto");
+        }
+    }
+}
