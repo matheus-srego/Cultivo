e13b219c2ef6cc6c1db541f350e5d9c87c5c7609
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-08 20:14:11 -0300

    Criando constante para endpoint

---
+++ Cultivo/Cultivo.Domain/Constants/Endpoints.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Domain.Constants
+{
+    public static class Endpoints
+    {
+        public const string HEADER_VALUE = "application/json";
+
+        public const string URL = "";
+
+        public const string API_USER = "api/user";
+    }
+}
