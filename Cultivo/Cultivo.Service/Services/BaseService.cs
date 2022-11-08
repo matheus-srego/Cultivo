92f0b5e646f4be5e650e510af017c4ab359ee8c1
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-08 20:17:36 -0300

    Adicionando atributos de validações às implementações de criar e editar

--- Cultivo/Cultivo.Service/Services/BaseService.cs
+++ Cultivo/Cultivo.Service/Services/BaseService.cs
@@ -1,6 +1,8 @@
-﻿using Cultivo.Domain.Interfaces.Repositories;
+﻿using Cultivo.Domain.Constants;
+using Cultivo.Domain.Interfaces.Repositories;
 using Cultivo.Domain.Interfaces.Services;
 using Cultivo.Domain.Models;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,10 +22,26 @@ namespace Cultivo.Service.Services
 
         public async Task<T> GetByIdAsync(int id) => await _baseRepository.GetByIdAsync(id);
 
-        public async Task<T> CreateAsync(T entity) => await _baseRepository.CreateAsync(entity);
+        public async Task<T> CreateAsync<TValidator>(T entity) where TValidator : AbstractValidator<T>
+        {
+            Validate(entity, Activator.CreateInstance<TValidator>());
+            return await _baseRepository.CreateAsync(entity);
+        }
 
-        public async Task<T> UpdateAsync(T entity) => await _baseRepository.UpdateAsync(entity);
+        public async Task<T> UpdateAsync<TValidator>(T entity) where TValidator : AbstractValidator<T>
+        {
+            Validate(entity, Activator.CreateInstance<TValidator>());
+            return await _baseRepository.UpdateAsync(entity);
+        }
 
         public async Task<T> DeleteAsync(int id) => await _baseRepository.DeleteAsync(id);
+
+        public void Validate(T entity, AbstractValidator<T> validator)
+        {
+            if (entity == null)
+                throw new Exception(Exceptions.MESSAGE_REGISTER);
+
+            validator.ValidateAndThrow(entity);
+        }
     }
 }
