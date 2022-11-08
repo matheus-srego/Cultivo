15d32621c0c92c011384c1a1131e9ef420ed4cab
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-08 20:15:02 -0300

    Criando constantes para viasualização de modelos (front-end)

---
+++ Cultivo/Cultivo.Domain/Constants/ViewModels.cs
@@ -0,0 +1,20 @@
+﻿namespace Cultivo.Domain.Constants
+{
+    public static class ViewModels
+    {
+        /* REQUIRED FIELDS MESSAGE for User View Model.. */
+        public const string REQUIRED_MESSAGE_NAME = "O campo 'nome' é obrigatório.";
+        public const string REQUIRED_MESSAGE_LASTNAME = "O campo 'sobrenome' é obrigatório.";
+        public const string REQUIRED_MESSAGE_EMAIL = "O campo 'e-mail' é obrigatório.";
+        public const string REQUIRED_MESSAGE_PASSWORD = "O campo 'senha' é orbigatório.";
+        /* --- */
+
+        /* DISPLAY FIELDS MESSAGE for User View Model.. */
+        public const string DISPLAY_FIELD_NAME = "Nome:";
+        public const string DISPLAY_FIELD_LASTNAME = "Sobrenome:";
+        public const string DISPLAY_FIELD_EMAIL = "E-mail:";
+        public const string DISPLAY_FIELD_PASSWORD = "Senha:";
+        public const string DISPLAY_FIELD_PHOTO = "Foto:";
+        /* --- */
+    }
+}
