fb91ba913fa3001184fc508d79d16dea9dc1d48b
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-08 20:14:32 -0300

    Criando constantes para exceptions

---
+++ Cultivo/Cultivo.Domain/Constants/Exceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Domain.Constants
+{
+    public static class Exceptions
+    {
+        public const string MESSAGE_REGISTER = "Registros não detectados.";
+    }
+}
