3653a5ec483ad7328938bd159be6694766db4245
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-08 19:26:06 -0300

    Deletando projeto MVC antigo..
    nem sei porque isso tava fora do projeto.. o.o

--- Cultivo.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cultivo.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
