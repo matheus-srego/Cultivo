84bddbc679c0de18d04b41a729e7e8ead62dda88
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-08 20:18:25 -0300

    Criando classe de validação para os atributos de usuário

---
+++ Cultivo/Cultivo.Service/Validations/UserValidator.cs
@@ -0,0 +1,33 @@
+﻿using Cultivo.Domain.Constants;
+using Cultivo.Domain.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivo.Service.Validations
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(model => model.Name)
+                .NotEmpty().WithMessage(Validators.MESSAGE_NAME_EMPTY)
+                .NotNull().WithMessage(Validators.MESSAGE_NAME_NULL);
+
+            RuleFor(model => model.LastName)
+                .NotEmpty().WithMessage(Validators.MESSAGE_LASTNAME_EMPTY)
+                .NotNull().WithMessage(Validators.MESSAGE_LASTNAME_NULL);
+
+            RuleFor(model => model.Email)
+                .NotEmpty().WithMessage(Validators.MESSAGE_EMAIL_EMPTY)
+                .NotNull().WithMessage(Validators.MESSAGE_EMAIL_NULL);
+
+            RuleFor(model => model.Password)
+                .NotEmpty().WithMessage(Validators.MESSAGE_PASSWORD_EMPTY)
+                .NotNull().WithMessage(Validators.MESSAGE_PASSWORD_NULL);
+        }
+    }
+}
