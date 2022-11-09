2c8fb53231fa11f8c3d2c29550365a55836e722c
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-08 23:19:07 -0300

    Removendo construtor do DTO

--- Cultivo/Cultivo.Domain/DTOs/NewUserDTO.cs
+++ Cultivo/Cultivo.Domain/DTOs/NewUserDTO.cs
@@ -13,14 +13,5 @@ namespace Cultivo.Domain.DTOs
         public string Email { get; set; }
         public string Password { get; set; }
         public string PhotoURL { get; set; }
-
-        public NewUserDTO(string name, string lastName, string email, string password, string photoURL)
-        {
-            Name = name;
-            LastName = lastName;
-            Email = email;
-            Password = password;
-            PhotoURL = photoURL;
-        }
     }
 }
