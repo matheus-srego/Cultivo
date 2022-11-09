822eddbd2c04fcf18286bab26728741e81e63e33
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-08 23:21:08 -0300

    Criando modelo de visualização abstrato

---
+++ Cultivo/Cultivo.Web/Models/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cultivo.Web.Models
+{
+    public class BaseViewModel
+    {
+        [Display(Name = "Identificador:")]
+        public virtual int Id { get; set; }
+    }
+}
