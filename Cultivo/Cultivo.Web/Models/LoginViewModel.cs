b7c2db2539aa6e11891b03668616ca645998fa73
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-08 23:21:15 -0300

    Criando modelo para login

---
+++ Cultivo/Cultivo.Web/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Cultivo.Domain.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cultivo.Web.Models
+{
+    public class LoginViewModel
+    {
+        [EmailAddress]
+        [Display(Name = ViewModels.DISPLAY_FIELD_EMAIL)]
+        [Required(ErrorMessage = ViewModels.REQUIRED_MESSAGE_EMAIL)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = ViewModels.DISPLAY_FIELD_PASSWORD)]
+        [Required(ErrorMessage = ViewModels.REQUIRED_MESSAGE_PASSWORD)]
+        public string Password { get; set; }
+    }
+}
