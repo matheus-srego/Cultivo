fcd72f191debe46179fcab77ac45e3390cd1ad0d
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-09 23:58:24 -0300

    Criando interface do método 'GetOneByCriteria' no repositório

--- Cultivo/Cultivo.Domain/Interfaces/Repositories/IBaseRepository.cs
+++ Cultivo/Cultivo.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Cultivo.Domain.Interfaces.Repositories
 {
     public interface IBaseRepository<T> where T : BaseEntity
     {
+        Task<T> GetOneByCriteria(Expression<Func<T, bool>> expression);
         Task<T> GetByIdAsync(int id);
         Task<T> CreateAsync(T entity);
         Task<T> UpdateAsync(T entity);
