d7b9dafcbeb15cc4b9e8ce365f570839530b60ec
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-09 23:57:55 -0300

    Adicionando novas mensagens de exceção

--- Cultivo/Cultivo.Domain/Constants/Exceptions.cs
+++ Cultivo/Cultivo.Domain/Constants/Exceptions.cs
@@ -9,5 +9,7 @@ namespace Cultivo.Domain.Constants
     public static class Exceptions
     {
         public const string MESSAGE_REGISTER = "Registros não detectados.";
+        public const string MESSAGE_USER_NOT_EXIST = "Credenciais não coincidem com o que há no banco.";
+        public const string MESSAGE_INCOMPLETE_INFORMATION = "As informações recebidas estão incompletas.";
     }
 }
