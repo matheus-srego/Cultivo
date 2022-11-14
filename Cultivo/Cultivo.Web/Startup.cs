cc4a8e777db8da785f53af2b86ec034ac5095845
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-11-13 22:33:56 -0300

    Criando autenticação no Web

---
+++ Cultivo/Cultivo.Web/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Cultivo.Domain.Constants;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Cultivo.Web
+{
+    public class Startup
+    {
+        protected readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public virtual void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddControllersWithViews();
+            
+            serviceCollection.AddSession(options =>
+            {
+                
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+
+            });
+
+            serviceCollection.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                             .AddJwtBearer(options =>
+                             {
+                                 options.TokenValidationParameters = new TokenValidationParameters
+                                 {
+                                     ValidateIssuer = false,
+                                     ValidateAudience = false,
+                                     ValidateLifetime = true,
+                                     ValidateIssuerSigningKey = true,
+                                     ValidIssuer = Endpoints.API_URL,
+                                     ValidAudience = Endpoints.API_URL,
+                                     IssuerSigningKey = new
+                                        SymmetricSecurityKey
+                                        (Encoding.UTF8.GetBytes(Endpoints.SECRET_KEY))
+                                 };
+                             });
+        }
+    }
+}
